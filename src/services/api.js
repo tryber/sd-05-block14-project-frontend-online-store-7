cce6a914adc4850ec81e4e30d82f4c52b258565c
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-24 16:28:49 -0300

    New api

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,19 @@
-// Requisições de API
+// new api
 
 export async function getCategories() {
-  fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((res) => res.json())
-    .then((result) => this.setState({ data: result }));
+  return fetch(
+    "https://api.mercadolibre.com/sites/MLB/categories"
+  ).then((res) => res.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((res) => res.json())
-    .then((result) => this.setState({ data: result }));
+  let Url;
+  if (categoryId)
+    Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  else if (query)
+    Url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  else
+    Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  const Result = fetch(Url).then((response) => response.json());
+  return Result;
 }
