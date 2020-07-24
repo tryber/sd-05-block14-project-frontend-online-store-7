f68c45d4a823d14ab84fef8577e942ccbf02246a
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-24 17:01:17 -0300

    codeclimate fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,12 @@
 export async function getCategories() {
-  return fetch(
-    "https://api.mercadolibre.com/sites/MLB/categories"
-  ).then((res) => res.json());
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories').then((res) => res.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   let Url;
-  if (categoryId)
-    Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  else if (query)
-    Url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  else
-    Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  if (categoryId) Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  else if (query) Url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  else Url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
   const Result = fetch(Url).then((response) => response.json());
   return Result;
 }
