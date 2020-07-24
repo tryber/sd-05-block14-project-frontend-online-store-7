54d0d1e0b3ce4d362ae11fb598e55667ecf6d94a
Christian Duessel <duessel@gmail.com>
2020-07-24 18:29:00 -0300

    first atempt of productCard

---
+++ src/components/productCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import movies from '../services/movieData';
+// import { getMovies } from '../services/movieAPI';
+// import PropTypes from 'prop-types';
+
+class ProductCard extends React.Component {
+  render() {
+    const { product: { title, price, thumbnail, id } } = this.props;
+
+    return (
+      <div data-testid="product">
+        <image src={thumbnail} alt={id} />
+        <h2>{title}</h2>
+        <p>{price}</p>
+        <div>
+          <p> Link to={`/product/${id}`}>VER DETALHES /Link </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductCard;
+
+// ProductCard.propTypes = {
+//  movie: PropTypes.shape({
+//    imagePath: PropTypes.string,
+//    title: PropTypes.string.isRequired,
+//    storyline: PropTypes.string.isRequired,
+//    id: PropTypes.number.isRequired,
+//  }).isRequired,
+// };
