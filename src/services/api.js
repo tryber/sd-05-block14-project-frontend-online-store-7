e19c9fc3fa9a9d6c2459f83073c111d185284436
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-24 15:21:22 -0300

    codeclimate adjusts

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 export async function getCategories() {
   fetch("https://api.mercadolibre.com/sites/MLB/categories")
-    .then(res => res.json())
-    .then(result => this.setState({ data: result }));
-  }
+    .then((res) => res.json())
+    .then((result) => this.setState({ data: result }));
+}
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
