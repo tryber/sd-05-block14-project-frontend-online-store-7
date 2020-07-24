322b0faa7419f50e0141e60104b882af26119416
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-24 15:31:24 -0300

    codeclimate adjusts

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,13 @@
 export async function getCategories() {
-  fetch("https://api.mercadolibre.com/sites/MLB/categories")
+  fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((res) => res.json())
     .then((result) => this.setState({ data: result }));
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`)
+  fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`
+  )
     .then((res) => res.json())
     .then((result) => this.setState({ data: result }));
 }
