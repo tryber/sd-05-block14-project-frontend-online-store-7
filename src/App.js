eb14b38e7c143130f00484cac8f9e68d53f7d4e1
marinabrcls <barcelos.marina@gmail.com>
2020-07-27 11:34:17 -0300

    router added to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+// falta importar todos os componentes
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="main-page">
+          <div className="search-bar">
+            <searchBar />
+          </div>
+          <div className="cart-button">
+            <Link data-testid="shopping-cart-button" to="/shoppingCart"> colocar imagem do botão aqui </Link>
+          </div>
+          <div className="categories">
+            <leftBarCategories />
+          </div>
+          <div className="products">
+            <Switch>
+              <Route exact path="/" component={productsLists} />
+              <Route path="/shoppingCart" component={shoppingCart} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
