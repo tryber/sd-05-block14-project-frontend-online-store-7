e8161e7269281aaae66ff3885ae059edf90b4b75
marinabrcls <barcelos.marina@gmail.com>
2020-07-27 15:31:01 -0300

    cc

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProductsLists from './productsLists';
 class SearchBar extends React.Component {
-  change = (e) => console.log(e.target.value);
   render() {
     return (
       // <input onChange={this.change}></input>
