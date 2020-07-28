aa7e6e7165476f4ab0b8c709173e31074be16776
lucasokazava <lucasokazava@gmail.com>
2020-07-28 17:09:22 -0300

    esqueleto carrinho de compras

---
+++ src/pages/ShoppingCart/CartItem.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class CartItem extends Component {
+  render() {
+    const {
+      increaseQuantity,
+      decreaseQuantity,
+      removeFromCart,
+      cartProduct,
+    } = this.props;
+    const { name, price, thumb, quantity } = cartProduct;
+
+    return (
+      <div>
+        <img src={thumb} alt={name} />
+        <p data-testid="shopping-cart-product-name">{name}</p>
+        <p data-testid="shopping-cart-product-quantity">
+          Quantidade: {quantity}
+        </p>
+        <button
+          type="button"
+          data-testid="product-decrease-quantity"
+          onClick={() => decreaseQuantity(cartProduct)}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={() => increaseQuantity(cartProduct)}
+        >
+          +
+        </button>
+        <p>Preço Total: R${price * quantity}</p>
+        <button type="button" onClick={() => removeFromCart(cartProduct)}>
+          Remover do carrinho
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CartItem;
