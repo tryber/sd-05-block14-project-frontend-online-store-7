5db379e51b86acabeeb107809aef68a8685f1001
marinabrcls <barcelos.marina@gmail.com>
2020-07-28 18:54:04 -0300

    esqueleto do CartList

---
+++ src/pages/ShoppingCart/CartList.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CartItem from './CartItem';
+
+class CartList extends Component {
+  render() {
+    const { increaseQuantity, decreaseQuantity, removeFromCart, cartItems, updateTotal } = this.props;
+
+    if (cartItems.length < 1) {
+      return (
+        <div>
+          <p data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </p>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        {cartItems.map((cartItem) => (
+          <CartItem
+            cartProduct={cartProduct}
+            increaseQuantity={increaseQuantity}
+            decreaseQuantity={decreaseQuantity}
+            removeFromCart={removeFromCart}
+            updateTotal={updateTotal}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CartList;
