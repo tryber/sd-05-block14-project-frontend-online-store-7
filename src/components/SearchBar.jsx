5f21c60b46ee03c8cf948ff2f3acca27e7d548a0
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-28 19:13:05 -0300

    button Search added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchText: '' }
+    this.onClickSearch = this.onClickSearch.bind(this);
+    this.resetInput = this.resetInput.bind(this);
+  }
+
+  onClickSearch(event) {
+    this.setState({searchText: event.target.value})
+  }
+
+  resetInput() {
+    this.setState({ searchText: '' })
+  }
+
+
+
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { inputValue } = this.state;
     return (
       <div>
-        <input type="text" value={searchText} onChange={onSearchTextChange} />
+        <form>
+          <input
+            value={inputValue}
+            data-testid="query-input"
+            onChange={this.onClickSearch}
+          />
+          <Link
+            to={{
+              pathname: '/',
+              state: { inputValue },
+            }}
+          >
+            <button
+              onClick={this.resetInput}
+              type="submit"
+              data-testid="query-button"
+            >
+              Pesquisar
+            </button>
+          </Link>
+        </form>
       </div>
     );
   }
+
 }
 
 export default SearchBar;
