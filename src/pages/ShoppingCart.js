dd93b3d3098575c49d6d79c0616c4111bd4a4cda
lucasokazava <lucasokazava@gmail.com>
2020-07-28 12:06:45 -0300

    mensagem do carrinho vazio

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ProductCard from './ProductCard'; *pode ser necessário pra lógica*
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
+}
+  
+export default ShoppingCart;
