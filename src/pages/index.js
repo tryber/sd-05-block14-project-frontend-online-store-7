0dc5293b404cdf867cc6b57d2dcf73bffa25985b
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 17:51:11 -0300

    now we are up to date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,67 @@
 import React from 'react';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ProductList from '../components/ProductsList';
 import SearchBar from '../components/SearchBar';
-
+import Categories from './components/Categories';
+import * as API from '../services/api';
+// import ShoppingCart from './ShoppingCart/ShoppingCart';
+​
+​
 class index extends Component {
   constructor(props) {
     super(props);
-    this.state = { products: '' };
-    // this.filterProducts = this.filterProducts.bind(this);
+    this.state = { search: null, category: null, value: null }; // , cart: []
+    this.searchCategory = this.searchCategory.bind(this);
+    this.searchValue = this.searchValue.bind(this);
+    // this.resetCategory = this.resetCategory.bind(this);
+    this.searchApi = this.searchApi.bind(this);
+    // this.addCart = this.addCart.bind(this);
+  } 
+​
+  async searchValue(sValue) {
+    await this.setState({ value: sValue });
+    this.searchApi();
+ }
+​
+  async searchCategory(selectedcategory) {
+    await this.setState({ category: selectedcategory });
+    this.searchApi();
   }
-
-// filterProducts() {}
-
+​
+  async searchApi() {
+    const { category, value } = this.state;
+    const apiResponse = await API.getProductsFromCategoryAndQuery(category, value);
+    this.setState({ search: apiResponse });
+  }
+​
+​
   render() {
+    const { search, value } = this.state; // cart
     return (
       <div>
         <div>
-          <SearchBar />
+          <SearchBar onClick={this.searchValue} />;
         </div>
-        <ProductList products={this.state.products} />
+        <div className="cart-button">
+          <Link data-testid="shopping-cart-button" to="/ShoppingCart">
+            <img
+              className="cart-img"
+              src="https://image.flaticon.com/icons/png/512/263/263142.png"
+              alt="Open shopping cart"
+              width="300px"
+            />
+          </Link>
+        </div>
+        <div>
+          <Categories onClick={this.searchCategory} value={value}/>
+        </div>  
+        <div>
+        <ProductList products={search} value={search} /> 
+        </div>
       </div>
     );
   }
 }
-
+​
 export default index;
