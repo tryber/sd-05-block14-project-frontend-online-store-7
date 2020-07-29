7cfd061329957cdcd656791d9fa3b6eba547c1b4
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 18:00:03 -0300

    debug index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,10 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ProductList from '../components/ProductsList';
 import SearchBar from '../components/SearchBar';
-import Categories from './components/Categories';
+import Categories from '../components/Categories';
 import * as API from '../services/api';
 // import ShoppingCart from './ShoppingCart/ShoppingCart';
-​
-​
+
 class index extends Component {
   constructor(props) {
     super(props);
@@ -17,51 +16,51 @@ class index extends Component {
     // this.resetCategory = this.resetCategory.bind(this);
     this.searchApi = this.searchApi.bind(this);
     // this.addCart = this.addCart.bind(this);
-  } 
-​
+  }
+
   async searchValue(sValue) {
     await this.setState({ value: sValue });
     this.searchApi();
- }
-​
+  }
+
   async searchCategory(selectedcategory) {
     await this.setState({ category: selectedcategory });
     this.searchApi();
   }
-​
+
   async searchApi() {
     const { category, value } = this.state;
     const apiResponse = await API.getProductsFromCategoryAndQuery(category, value);
     this.setState({ search: apiResponse });
   }
-​
-​
+
+
   render() {
     const { search, value } = this.state; // cart
     return (
       <div>
         <div>
-          <SearchBar onClick={this.searchValue} />;
+          <SearchBar onClick={this.searchValue} />
         </div>
         <div className="cart-button">
           <Link data-testid="shopping-cart-button" to="/ShoppingCart">
             <img
               className="cart-img"
               src="https://image.flaticon.com/icons/png/512/263/263142.png"
-              alt="Open shopping cart"
-              width="300px"
+              alt="Shopping cart icon"
+              width="50px"
             />
           </Link>
         </div>
         <div>
-          <Categories onClick={this.searchCategory} value={value}/>
-        </div>  
+          <Categories onClick={this.searchCategory} value={value} />
+        </div>
         <div>
-        <ProductList products={search} value={search} /> 
+          <ProductList products={search} value={search} />
         </div>
       </div>
     );
   }
 }
-​
+
 export default index;
