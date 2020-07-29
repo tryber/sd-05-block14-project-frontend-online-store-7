7bcfc16238fe1ad6d18cb1ef8699eec5bb742929
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-07-28 23:10:29 -0300

    Function added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { Component } from 'react';
 import ProductList from '../components/ProductsList';
 import SearchBar from '../components/SearchBar';
-
+import * as API from '../services/api';
 class index extends Component {
   constructor(props) {
     super(props);
-    this.state = { products: '' };
-    // this.filterProducts = this.filterProducts.bind(this);
+    this.state = { 
+      products: [],
+      category: '',
+      SearchText: '',
+    };
+    this.onSearchResults = this.onSearchResults.bind(this);
   }
 
-// filterProducts() {}
+  onSearchResults = async text => {
+    const response = await API.getProductsFromCategoryAndQuery(null, text)
+      this.setState({ products: response})
+  }
 
   render() {
     return (
       <div>
         <div>
-          <SearchBar />
+          <SearchBar onSubmit={this.onSearchResults}/>
         </div>
         <ProductList products={this.state.products} />
       </div>
@@ -24,4 +31,4 @@ class index extends Component {
   }
 }
 
-export default index;
+export default index; 
