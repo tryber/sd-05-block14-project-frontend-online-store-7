e476f8e3385c14cf1c6bb79d81a8acc346b187ed
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 16:12:05 -0300

    changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,109 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
-import index from './pages/index';
-import Categories from './components/Categories';
-import SearchBar from './components/SearchBar';
 import ProductDetails from './pages/ProductDetails';
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
+import Home from './pages/index';
 
-// falta importar alguns componentes e páginas
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = { cartItems: [] };
+    this.increaseQuantityHandler = this.increaseQuantityHandler.bind(this);
+    this.decreaseQuantityHandler = this.increaseQuantityHandler.bind(this);
+    this.removeFromCartHandler = this.increaseQuantityHandler.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div className="products">
+  increaseQuantityHandler(newCartItem) {
+    this.setState((state) => ({
+      cartItems: state.cartItems.some(
+        (product) => product.id === newCartItem.id
+      )
+        ? state.cartItems.map((product) => {
+            if (product.id === newCartItem.id) {
+              return {
+                ...product,
+                quantity: product.quantity + 1,
+              };
+            }
+            return product;
+          })
+        : [...state.cartItems, { ...newCartItem, quantity: 1 }],
+    }));
+  }
+
+  decreaseQuantityHandler(subCartItem) {
+    this.setState((state) => ({
+      cartItems: state.cartItems.some(
+        (product) => product.id === subCartItem.id
+      )
+        ? state.cartItems.map((product) => {
+            if (product.id === subCartItem.id) {
+              return {
+                ...product,
+                quantity: product.quantity + 1,
+              };
+            }
+            return product;
+          })
+        : [...state.cartItems, { ...subCartItem, quantity: 1 }],
+    }));
+  }
+
+  removeFromCart(rmvCartItem) {
+    this.setState((state) => {
+      const newCartItems = [...state.cartItems];
+      const index = newCartItems.findIndex(
+        (product) => product.id === rmvCartItem.id
+      );
+      newCartItems.splice(index, 1);
+      return { cartItems: newCartItems };
+    });
+  }
+
+  routeCreator(page, path) {
+    const PageName = page;
+    const { cartItems } = this.state;
+    return (
+      <Route
+        path={path}
+        render={(props) => (
+          <PageName
+            {...props}
+            increaseQuantity={this.increaseQuantityHandler}
+            decreaseQuantity={this.decreaseQuantityHandler}
+            removeFromCart={this.removeFromCart}
+            cartItems={cartItems}
+          />
+        )}
+      />
+    );
+  }
+
+/* tranferir para homepage este link:
+<Link data-testid="shopping-cart-button" to="/shoppingCart">
+  <img
+    className="cart-img"
+    src="https://image.flaticon.com/icons/png/512/263/263142.png"
+    alt="Open shopping cart"
+    width="50px"
+  />
+</Link> */
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={index} />
-            {/*  deveria fazer um botão com link para este path e não um Route, senão quebra o código
-            <Route exact path="/:id" component={ProductDetails} />
-            */}
-            <Route path="/ShoppingCart" component={ShoppingCart} />
+            {this.routeCreator(ProductDetails, '/product/:productID')}
+            {this.routeCreator(ShoppingCart, '/shoppingCart')}
+            {this.routeCreator(Home)}
           </Switch>
-        </div>
-        <div>
-          <SearchBar />
-        </div>
-        <div>
-          <Categories />
-        </div>
-        <div className="cart-button">
-          <Link data-testid="shopping-cart-button" to="/ShoppingCart">
-            <img
-              className="cart-img"
-              src="https://image.flaticon.com/icons/png/512/263/263142.png"
-              alt="Open shopping cart"
-              width="50px"
-            />
-          </Link>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
+
