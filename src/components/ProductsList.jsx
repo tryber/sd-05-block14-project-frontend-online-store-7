f0acb534d98ebd3d937c64d50985a0f57b44ae74
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 22:21:01 -0300

    changes on productsList

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
-// import getmovies from '../services/movieAPI';
-// import * as API from '../services/api';
 
 class ProductList extends Component {
   render() {
-    const { products } = this.props; // Incluir add to cart aqui
-    if (products === null) {
+    const { products, addToCart } = this.props;
+    if (products.length < 1) {
       return (
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
@@ -18,7 +15,7 @@ class ProductList extends Component {
     return (
       <div>
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} product={product} addToCart={addToCart} />
         ))}
       </div>
     );
