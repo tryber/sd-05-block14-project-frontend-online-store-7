2f5fc3dd6bb103bf6d5e087c1fe7329d827105db
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 22:18:41 -0300

    changes on productCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,48 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import movies from '../services/movieData';
-// import { getMovies } from '../services/movieAPI';
-// import PropTypes from 'prop-types';
 
-// AVISO: ainda falta o botão e não sei se estamos puxando as props corretamente
-
-class ProductCard extends React.Component {
+class ProductCard extends Component {
   render() {
-    const { product, increaseQuantity } = this.props;
-    const { title, price, thumbnail, id } = product;
+    const { product, addToCart } = this.props;
+    const { title, price, thumbnail } = product;
 
     return (
       <div data-testid="product">
-        <img src={thumbnail} alt={id} />
         <h2>{title}</h2>
-        <p>{price}</p>
-        <div>
-          <button
-            data-testid="product-add-to-cart"
-            type="button"
-            onClick={() => increaseQuantity(product)}
-          >
-            COMPRAR
-          </button>
-          <Link
+        <img src={thumbnail} alt={title} />
+        <p>Preço: R${price}</p>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          onClick={() => addToCart(product)}
+        >
+          Adicionar ao carrinho
+        </button>
+        <Link
             data-testid="product-detail-link"
             to={`/products/${this.props.product.id}`}
-          >
-            VER DETALHES
-          </Link>
+        >
+          Ver detalhes
+        </Link>
         </div>
-      </div>
     );
   }
 }
 
 export default ProductCard;
-
-// ProductCard.propTypes = {
-//  movie: PropTypes.shape({
-//    imagePath: PropTypes.string,
-//    title: PropTypes.string.isRequired,
-//    storyline: PropTypes.string.isRequired,
-//    id: PropTypes.number.isRequired,
-//  }).isRequired,
-// };
