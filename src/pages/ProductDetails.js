64fd5d8ad87425d481cf9ea5afac43a341a2d123
marinabrcls <barcelos.marina@gmail.com>
2020-07-29 22:46:51 -0300

    changes on ProductDetails (not much)

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,10 +1,5 @@
-// Adicione o atributo data-testid com o valor product-detail-name
-// no elemento que possui o nome do produto na tela de detalhes.
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import * as API from '../services/api';
-// import { Loading } from '../components';
 
 class ProductDetails extends Component {
   constructor(props) {
@@ -14,44 +9,39 @@ class ProductDetails extends Component {
 
   componentDidMount() {
     const id = this.props.match.params.id;
-    // const { id } = this.props;
     fetch(`https://api.mercadolibre.com/items/${id}`)
       .then((data) => data.json())
       .then((result) => this.setState({ product: result }));
   }
 
   render() {
-    const { product } = this.state;
     const { title, price, thumbnail } = product;
-    const { increaseQuantity } = this.props;
+    const { product } = this.state;
+    const { addToCart } = this.props;
     return (
-      <section>
-        <img alt="Fotenha" src={thumbnail} />
-        <p data-testid="product-detail-name">{`Produto: ${title}`}</p>
-        <p>{`Preço: R$ ${price}`}</p>
-        <div>
+      <div className="details-container">
+        <div className="go-back">
+          <Link to="/">VOLTAR</Link>
+        </div>
+        <div className="product-detail">
+          <h2 data-testid="product-detail-name">{`Produto: ${title}`}</h2>
+          <img alt={title} src={thumbnail} />
+          <p>{`Preço: R$ ${price}`}</p>
           <button
             data-testid="product-detail-add-to-cart"
             type="button"
-            onClick={() => increaseQuantity(product)}
-          >COMPRAR</button>
+            onClick={() => addToCart(product)}
+          >
+            Adicionar ao carrinho
+          </button>
         </div>
-        <div>
-          <Link to="/">VOLTAR</Link>
-        </div>
-        <div>
+        <div className="evaluate-product">
           <label htmlFor="evaluation">Avalie esse produto:</label>
           <input id="evaluation" type="text" data-testid="product-detail-evaluation" />
         </div>
-      </section>
+      </div>
     );
   }
 }
 
 export default ProductDetails;
-
-// MovieDetails.propTypes = {
-//   match: PropTypes.shape({
-//     params: PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired,
-//   }).isRequired,
-// };
